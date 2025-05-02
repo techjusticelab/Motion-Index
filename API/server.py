9d4f5642a59f3e6cd3649cbb59206fef0f898d9d
techjusticelab <info@techjusticelab.org>
2025-05-01 23:53:01 -0700

    Update server.py

--- API/server.py
+++ API/server.py
@@ -37,7 +37,7 @@ app = FastAPI(
 )
 
 origins = [
-    "https://api.motionindex.techjusticelab.org",
+    "http://api.motionindex.techjusticelab.org",
     "http://localhost",
     "http://localhost:8080",
 ]
